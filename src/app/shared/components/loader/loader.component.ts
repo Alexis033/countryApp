4ae697473031d9b04f4159e0a25f571a6d3a5cdd
Alexis033 <10guevarafm1102@gmail.com>
2024-07-12 13:43:06 -0500

    update loader.component

--- src/app/shared/components/loader/loader.component.ts
+++ src/app/shared/components/loader/loader.component.ts
@@ -3,9 +3,12 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'shared-loader',
   templateUrl: './loader.component.html',
-  styles: ``,
+  styleUrls: ['./loader.component.css'],
 })
 export class LoaderComponent {
   @Input()
   public color: string = '#000';
+
+  @Input()
+  public text: string = '';
 }
